8825cff5d99f2ef1d4661d6c7219d19f9ea4435d
Andreas Sauer <18749753+Hanibal1963@users.noreply.github.com>
2024-07-17 06:19:10 +0200

    Update KategorieDescriptionConstants.vb

--- CommonCodes/SevenSegmentControl/KategorieDescriptionConstants.vb
+++ CommonCodes/SevenSegmentControl/KategorieDescriptionConstants.vb
@@ -1,1 +1,13 @@
+' ****************************************************************************************************************
+' KategorieDesciptionConstants.vb
+' © 2024 by Andreas Sauer
+' ****************************************************************************************************************
+'
 
+
+Friend Module KategorieDesciptionConstants
+
+    Friend Const Category_Appearance = "Appearance"
+    Friend Const Category_Behavior = "Behavior"
+
+End Module
