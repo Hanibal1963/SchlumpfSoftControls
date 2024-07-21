27875972cd2214d3c5e38b16f382fcc71699f66e
Andreas Sauer <18749753+Hanibal1963@users.noreply.github.com>
2024-07-21 11:49:13 +0200

    Aktualisieren von ClassDescriptionConstants.vb

--- CommonCodes/ClassDescriptionConstants.vb
+++ CommonCodes/ClassDescriptionConstants.vb
@@ -16,36 +16,31 @@ Friend Class ClassDescriptionConstants
     ''' <summary>
     ''' Kurzbeschreibung des IniFile Verwaltungscontrols
     ''' </summary>
-    Public Const IniFile =
-        "Control zum Verwalten von INI - Dateien"
+    Public Const IniFile ="Control zum Verwalten von INI - Dateien"
 
 
     ''' <summary>
     ''' Kurzbeschreibung des IniFile Kommentareditor Controls
     ''' </summary>
-    Public Const IniFileCommentEdit =
-        "Steuerelement zum Anzeigen und Bearbeiten des Datei- oder Abschnitts- Kommentars einer INI - Datei."
+    Public Const IniFileCommentEdit ="Steuerelement zum Anzeigen und Bearbeiten des Datei- oder Abschnitts- Kommentars einer INI - Datei."
 
 
     ''' <summary>
     ''' Kurzbeschreibung des IniFile Dateiihaltsanzeige Controls
     ''' </summary>
-    Public Const IniFileContentView =
-        "Steuerelement zum Anzeigen des Dateiinhaltes."
+    Public Const IniFileContentView ="Steuerelement zum Anzeigen des Dateiinhaltes."
 
 
     ''' <summary>
     ''' Kurzbeschreibung des IniFile Eintragseditor Controls
     ''' </summary>
-    Public Const IniFileEntryValueEdit =
-        "Steuerelement zum Anzeigen und Bearbeiten der Einträge eines Abschnitts einer INI - Datei."
+    Public Const IniFileEntryValueEdit ="Steuerelement zum Anzeigen und Bearbeiten der Einträge eines Abschnitts einer INI - Datei."
 
 
     ''' <summary>
     ''' Kurzbeschreibung des IniFile Listeneditor Controls
     ''' </summary>
-    Public Const IniFileListEdit =
-        "Steuerelement zum Anzeigen und Bearbeiten der Abschnitts- oder Eintrags- Liste einer INI - Datei."
+    Public Const IniFileListEdit ="Steuerelement zum Anzeigen und Bearbeiten der Abschnitts- oder Eintrags- Liste einer INI - Datei."
 
 
     '''' <summary>
@@ -57,15 +52,13 @@ Friend Class ClassDescriptionConstants
     ''' <summary>
     ''' Kurzbeschreibung des SevSegSingleDigit Control
     ''' </summary>
-    Public Const SevSegSingleDigit =
-        "Control zum Anzeigen einer Ziffer als 7-Segmentanzeige."
+    Public Const SevSegSingleDigit ="Control zum Anzeigen einer Ziffer als 7-Segmentanzeige."
 
 
     ''' <summary>
     ''' Kurzbeschreibung des SevSegMultiDigit Control
     ''' </summary>
-    Public Const SevSegMultiDigit =
-        "Control zum Anzeigen einer Ziffernfolge als 7-Segmentanzeige."
+    Public Const SevSegMultiDigit ="Control zum Anzeigen einer Ziffernfolge als 7-Segmentanzeige."
 
 
 
@@ -78,8 +71,7 @@ Friend Class ClassDescriptionConstants
     ''' <summary>
     ''' 
     ''' </summary>
-    Public Const Wizard =
-        "Ein Control zum erstellen eines Assistenen"
+    Public Const Wizard ="Ein Control zum erstellen eines Assistenen"
 
 
 End Class
