8dab02d7e7858156efdf7ccb93593b6c6c92bce8
Andreas Sauer <18749753+Hanibal1963@users.noreply.github.com>
2024-09-29 10:12:19 +0200

    Refactor und neue Ereignisbehandlungen hinzugefügt
    - `Imports System`-Anweisung entfernt
    - Vererbung von `IniFileCommentEdit` in einer Zeile deklariert
    - Neue Abschnitte für interne Variablen und Ereignisse hinzugefügt
    - Öffentliche und interne Ereignisse getrennt
    - Initialisierung von `_TitelText` im Konstruktor entfernt
    - `TitelText` und `Comment` Eigenschaften lösen Ereignisse aus
    - Neue Methoden für Ereignisbehandlungen hinzugefügt
    - `Button_Click` Methode deaktiviert Button nach Ereignis
    - `IniFileCommentEdit_PropCommentChanged` aktualisiert Textbox und deaktiviert Button
    - `IniFileCommentEdit_TitelTextChanged` aktualisiert GroupBox Titeltext

--- IniFileControl/IniFileCommentEdit.vb
+++ IniFileControl/IniFileCommentEdit.vb
@@ -4,13 +4,11 @@
 ' ****************************************************************************************************************
 '
 
-
 Imports System
 Imports System.ComponentModel
 Imports System.Drawing
 Imports System.Linq
 Imports System.Windows.Forms
-
 
 ''' <summary>
 ''' Steuerelement zum Anzeigen und Bearbeiten des Datei- oder Abschnitts- Kommentars einer INI - Datei.
@@ -19,18 +17,16 @@ Imports System.Windows.Forms
 <MyDescription("ClassDescriptionCommentEdit")>
 <ToolboxItem(True)>
 <ToolboxBitmap(GetType(IniFileCommentEdit), "IniFileCommentEdit.bmp")>
-Public Class IniFileCommentEdit
+Public Class IniFileCommentEdit : Inherits UserControl
 
-
-    Inherits UserControl
-
+#Region "Definition der internen Eigenschaftsvariablen"
 
     Private _Lines As String() = {""}
     Private _TitelText As String
 
+#End Region
 
-#Region "Definition der Ereignisse"
-
+#Region "Definition der öffentlichenEreignisse"
 
     ''' <summary>
     ''' Wird ausgelöst wenn sich der Kommentartext geändert hat.
@@ -38,27 +34,30 @@ Public Class IniFileCommentEdit
     <MyDescription("CommentChangedDescription")>
     Public Event CommentChanged(sender As Object, e As EventArgs)
 
+#End Region
 
+#Region "Definition der internen Ereignisse"
+
+    ''' <summary>
+    ''' Wir ausgelöst wenn sich die Eigenschaft Comment geändert hat.
+    ''' </summary>
     Private Event PropCommentChanged()
-    Private Event TitelTextChanged()
 
+    ''' <summary>
+    ''' Wird ausgelöst wenn sich die Eigenschaft TitelText geändert hat.
+    ''' </summary>
+    Private Event TitelTextChanged()
 
 #End Region
 
-
     Public Sub New()
-
         ' Dieser Aufruf ist für den Designer erforderlich.
         Me.InitializeComponent()
-
         ' Fügen Sie Initialisierungen nach dem InitializeComponent()-Aufruf hinzu.
         Me._TitelText = Me.GroupBox.Text
-
     End Sub
 
-
 #Region "Definition der neuen Eigenschaften"
-
 
     ''' <summary>
     ''' Gibt den Text der Titelzeile zurück oder legt diesen fest.
@@ -71,11 +70,14 @@ Public Class IniFileCommentEdit
         Set(value As String)
             ' hat sich der Wert geändert?
             If Me._TitelText <> value Then
+                ' Wert setzen
                 Me._TitelText = value
+                ' Ereignis auslösen
                 RaiseEvent TitelTextChanged()
             End If
         End Set
         Get
+            ' Wert zurückgeben
             Return Me._TitelText
         End Get
     End Property
@@ -89,12 +91,15 @@ Public Class IniFileCommentEdit
     <MyDescription("CommentDescription")>
     Public Property Comment As String()
         Get
+            ' Wert zurückgeben
             Return Me._Lines
         End Get
         Set
             ' hat sich der Wert geändert?
             If Not Me._Lines.SequenceEqual(Value) Then
+                ' Wert setzen
                 Me._Lines = Value
+                ' Ereignis auslösen
                 RaiseEvent PropCommentChanged()
             End If
         End Set
@@ -103,21 +108,26 @@ Public Class IniFileCommentEdit
 
 #End Region
 
-
 #Region "Definition der internen Ereignisbehandlungen"
 
-
+    ''' <summary>
+    ''' Wird ausgelöst wenn der Button geklickt wird.
+    ''' </summary>
     Private Sub Button_Click(sender As Object, e As EventArgs) Handles _
         Button.Click
 
+        ' geänderten Kommentar übernehmen
         Me._Lines = Me.TextBox.Lines
-
-        'Ereignis auslösen
+        ' Button deaktivieren
+        Me.Button.Enabled = False
+        ' Ereignis auslösen
         RaiseEvent CommentChanged(Me, EventArgs.Empty)
 
     End Sub
 
-
+    ''' <summary>
+    ''' Wird ausgelöst wenn sich der Text in der Textbox geändert hat.
+    ''' </summary>
     Private Sub TextBox_TextChanged(sender As Object, e As EventArgs) Handles _
         TextBox.TextChanged
 
@@ -126,24 +136,30 @@ Public Class IniFileCommentEdit
 
     End Sub
 
-
+    ''' <summary>
+    ''' Wird ausgelöst wenn sich die Eigenschaft Comment geändert hat.
+    ''' </summary>
     Private Sub IniFileCommentEdit_PropCommentChanged() Handles _
         Me.PropCommentChanged
 
+        ' Kommentarzeilen in die Textbox eintragen
         Me.TextBox.Lines = Me._Lines
+        ' Button deaktivieren
         Me.Button.Enabled = False
 
     End Sub
 
+    ''' <summary>
+    '''  Wird ausgelöst wenn sich die Eigenschaft TitelText geändert hat.
+    ''' </summary>
     Private Sub IniFileCommentEdit_TitelTextChanged() Handles _
         Me.TitelTextChanged
 
+        ' Titeltext setzen
         Me.GroupBox.Text = Me._TitelText
 
     End Sub
 
-
 #End Region
-
 
 End Class
