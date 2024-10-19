a8e442e7dad69645e9e001b35d9b1d776da0d29a
Andreas Sauer <18749753+Hanibal1963@users.noreply.github.com>
2024-10-19 15:39:33 +0200

    Version auf 3.2024.10.19 aktualisiert, ReleaseNotes verbessert
    Die Versionsnummern in `AssemblyInfo.vb` und `source.extension.vsixmanifest` wurden auf 3.2024.10.19 aktualisiert.
    
    In `ReleaseNotes.html` wurde das `xmlns` Attribut entfernt und ein CSS-Style-Block hinzugefügt, um die Schriftart und Hintergrundfarbe des `<body>` Tags zu definieren.

--- SchlumpfSoftControls/My Project/AssemblyInfo.vb
+++ SchlumpfSoftControls/My Project/AssemblyInfo.vb
@@ -36,7 +36,7 @@ Imports Microsoft.VisualBasic
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 ' [assembly: AssemblyVersion("1.0.*")]
-<Assembly: AssemblyVersion("3.2024.10.12")>
-<Assembly: AssemblyFileVersion("3.2024.10.12")>
+<Assembly: AssemblyVersion("3.2024.10.19")>
+<Assembly: AssemblyFileVersion("3.2024.10.19")>
 
-<assembly: AssemblyInformationalVersion("0.2024.10.12")>
+<assembly: AssemblyInformationalVersion("0.2024.10.19")>
