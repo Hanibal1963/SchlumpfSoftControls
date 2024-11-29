8c5f42608a9f3fdb7032ae6a9f419ade974e9940
Andreas Sauer <18749753+Hanibal1963@users.noreply.github.com>
2024-11-29 20:10:03 +0100

    Neue Funktionen für spezielle Ordner hinzugefügt
    - Neue Funktion `GetSpezialFolderPath` hinzugefügt, die den vollen Pfad eines speziellen Ordners basierend auf dem übergebenen Ordnernamen zurückgibt. Diese Funktion verwendet `Environment.GetFolderPath` und `Environment.SpecialFolder`, um den Pfad zu ermitteln.
    - Neue Funktion `GetSpezialFolderName` hinzugefügt, die den einfachen Namen eines speziellen Ordners basierend auf dem übergebenen `Environment.SpecialFolder` zurückgibt. Diese Funktion verwendet eine `Select Case`-Anweisung, um den entsprechenden Namen zu ermitteln.
    - Neue Funktion `GetSpezialFolderImageKey` hinzugefügt, die den Bildschlüssel eines speziellen Ordners basierend auf dem übergebenen `Environment.SpecialFolder` zurückgibt. Diese Funktion verwendet ebenfalls eine `Select Case`-Anweisung, um den entsprechenden Bildschlüssel zu ermitteln.

--- ExplorerTreeViewControl/NodeHelpers.vb
+++ ExplorerTreeViewControl/NodeHelpers.vb
@@ -56,60 +56,87 @@ Friend Module NodeHelpers
     ''' Gibt den vollen Pfade des speziellen Ordners zurück
     ''' </summary>
     Friend Function GetSpezialFolderPath(SpezialFolderName As String) As String
+
         'feststellen des speziellen Ordnername
         Select Case SpezialFolderName
+
             Case "Desktop"
                 Return $"{Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) & "\"}"
+
             Case "Dokumente"
                 Return $"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) & "\"}"
+
             Case "Bilder"
                 Return $"{Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) & "\"}"
+
             Case "Musik"
                 Return $"{Environment.GetFolderPath(Environment.SpecialFolder.MyMusic) & "\"}"
+
             Case "Videos"
                 Return $"{Environment.GetFolderPath(Environment.SpecialFolder.MyVideos) & "\"}"
+
             Case Else
                 Return $"{SpezialFolderName}"
+
         End Select
+
     End Function
 
     ''' <summary>
     ''' Gibt den einfachen Name eines speziellen Ordners zurück
     ''' </summary>
     Friend Function GetSpezialFolderName(Folder As Environment.SpecialFolder) As String
+
         'feststellen des Spezialordners
         Select Case Folder
+
             Case Environment.SpecialFolder.DesktopDirectory
                 Return $"Desktop"
+
             Case Environment.SpecialFolder.MyDocuments
                 Return $"Dokumente"
+
             Case Environment.SpecialFolder.MyPictures
                 Return $"Bilder"
+
             Case Environment.SpecialFolder.MyMusic
                 Return $"Musik"
+
             Case Environment.SpecialFolder.MyVideos
                 Return $"Videos"
+
             Case Else
                 Return $"{Folder}"
+
         End Select
+
     End Function
 
     Friend Function GetSpezialFolderImageKey(Folder As Environment.SpecialFolder) As String
+
         'feststellen des Spezialordners
         Select Case Folder
+
             Case Environment.SpecialFolder.DesktopDirectory
                 Return "DesktopFolder"
+
             Case Environment.SpecialFolder.MyDocuments
                 Return "DocumentsFolder"
+
             Case Environment.SpecialFolder.MyPictures
                 Return "PicturesFolder"
+
             Case Environment.SpecialFolder.MyMusic
                 Return "MusicFolder"
+
             Case Environment.SpecialFolder.MyVideos
                 Return "VideoFolder"
+
             Case Else
                 Return Folder.ToString
+
         End Select
+
     End Function
 
 End Module
