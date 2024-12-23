9218c3814a553641506fbad2664527552f8c0370
Andreas Sauer <18749753+Hanibal1963@users.noreply.github.com>
2024-12-23 15:39:57 +0100

    DriveWatcher: Ereignisbehandlung und Ressourcenverwaltung
    Die Imports von `SchlumpfSoft.Attribute` wurden in `DriveWatcher.vb` hinzugefügt.
    Die Klasse `DriveWatcher` wurde umstrukturiert und erweitert:
      * Vererbung von `Component` in separate Zeile verschoben.
      * Neues privates Feld `NatForm` vom Typ `NativeForm` hinzugefügt.
      * Event-Handler-Methoden `NatForm_DriveAdded` und `NatForm_DriveRemoved` hinzugefügt.
      * Methode `Dispose` erweitert, um `components` und `NatForm` freizugeben.
      * Neue Strukturen `DEV_BROADCAST_HDR`, `DEV_BROADCAST_OEM` und `DEV_BROADCAST_VOLUME` hinzugefügt.
      * Methode `WndProc` überschrieben, um auf `WM_DEVICECHANGE`-Nachrichten zu reagieren.
      * Methoden `HandleHeader`, `HandleVolume` und `HandleOEM` hinzugefügt.
      * Methode `DriveFromMask` hinzugefügt.
      * Konstruktoren und `Dispose`-Methoden angepasst.
    
    In `DriveWatcherControl.vbproj` wurden unnötige `None`-Elemente entfernt und `Compile`-Elemente für `DriveAddedEventArgs.vb` und `DriveRemovedEventArgs.vb` hinzugefügt.
    
    In `AssemblyInfo.vb` wurden die Assembly-Version und die Assembly-Dateiversion von `2.2024.12.07` auf `2.2024.12.23` aktualisiert.

--- DriveWatcherControl/DriveWatcher.vb
+++ DriveWatcherControl/DriveWatcher.vb
@@ -23,10 +23,13 @@ Imports SchlumpfSoft.Attribute
 <ToolboxItem(True)>
 <Description("Steuerelement um die Laufwerke zu überwachen.")>
 <ToolboxBitmap(GetType(DriveWatcher), "DriveWatcher.bmp")>
-Public Class DriveWatcher : Inherits Component
+Public Class DriveWatcher
+
+    Inherits Component
 
     ' Wird vom Komponenten-Designer benötigt.
     Private ReadOnly components As IContainer
+
     ' Internes Formular welches die Meldungen empfängt.
     Private WithEvents NatForm As New NativeForm
 
@@ -61,6 +64,7 @@ Public Class DriveWatcher : Inherits Component
     ''' Wird ausgelöst wenn ein Laufwerk hinzugefügt wurde
     ''' </summary>
     Private Sub NatForm_DriveAdded(sender As Object, e As DriveInfo) Handles NatForm.DriveAdded
+
         Dim arg As New DriveAddedEventArgs With {
             .DriveName = e.Name,
             .VolumeLabel = e.VolumeLabel,
@@ -71,14 +75,17 @@ Public Class DriveWatcher : Inherits Component
             .DriveType = e.DriveType,
             .IsReady = e.IsReady}
         RaiseEvent DriveAdded(Me, arg)
+
     End Sub
 
     ''' <summary>
     ''' Wird ausgelöst wenn ein Laufwerk entfern wurde
     ''' </summary>
     Private Sub NatForm_DriveRemoved(sender As Object, e As DriveInfo) Handles NatForm.DriveRemoved
+
         Dim arg As New DriveRemovedEventArgs With {.DriveName = e.Name}
         RaiseEvent DriveRemoved(Me, arg)
+
     End Sub
 
     ''' <summary>
@@ -94,9 +101,11 @@ Public Class DriveWatcher : Inherits Component
 
     <Diagnostics.DebuggerNonUserCode()>
     Public Sub New(container As IContainer)
+
         MyClass.New()
         'Erforderlich für die Unterstützung des Windows.Forms-Klassenkompositions-Designers
         If container IsNot Nothing Then container.Add(Me)
+
     End Sub
 
     ''' <summary>
@@ -105,6 +114,7 @@ Public Class DriveWatcher : Inherits Component
     ''' <param name="disposing"></param>
     <Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(disposing As Boolean)
+
         Try
             If disposing Then
                 If Me.components IsNot Nothing Then Me.components.Dispose()
@@ -113,6 +123,7 @@ Public Class DriveWatcher : Inherits Component
         Finally
             MyBase.Dispose(disposing)
         End Try
+
     End Sub
 
     ''' <summary>
@@ -190,9 +201,11 @@ Public Class DriveWatcher : Inherits Component
         ''' https://learn.microsoft.com/de-de/windows/win32/api/dbt/ns-dbt-dev_broadcast_hdr
         ''' </remarks>
         Private Structure DEV_BROADCAST_HDR
+
             Public dbch_size As Integer
             Public dbch_devicetype As Integer
             Public dbch_reserved As Integer
+
         End Structure
 
         ''' <summary>
@@ -202,11 +215,13 @@ Public Class DriveWatcher : Inherits Component
         ''' https://learn.microsoft.com/de-de/windows/win32/api/dbt/ns-dbt-dev_broadcast_oem
         ''' </remarks>
         Private Structure DEV_BROADCAST_OEM
+
             Public dbco_size As Integer
             Public dbco_devicetype As Integer
             Public dbco_reserved As Integer
             Public dbco_identifier As Integer
             Public dbco_suppfunc As Integer
+
         End Structure
 
         ''' <summary>
@@ -216,104 +231,156 @@ Public Class DriveWatcher : Inherits Component
         ''' https://learn.microsoft.com/de-de/windows/win32/api/dbt/ns-dbt-dev_broadcast_volume
         ''' </remarks>
         Private Structure DEV_BROADCAST_VOLUME
+
             Public dbch_size As Integer
             Public dbch_devicetype As Integer
             Public dbch_reserved As Integer
             Public dbcv_unitmask As Integer
             Public dbcv_flags As Short
+
         End Structure
 
         'Dies ist der Dreh- und Angelpunkt der Klasse. - Hier bekommen wir die Messages mit.
         'In unserm Fall interessiert uns nur die WM_DeviceChange-Nachricht
         Protected Overrides Sub WndProc(ByRef m As Message)
+
             If m.Msg = WM_DEVICECHANGE Then Me.HandleHeader(m)
             MyBase.WndProc(m)
+
         End Sub
 
         'Hier schauen wir erst mal in den Header und verzweigen dementsprechend
         Private Sub HandleHeader(ByRef m As Message)
+
             Dim header As DEV_BROADCAST_HDR
             Dim objHeader As Object = m.GetLParam(header.GetType)
+
             If Not Microsoft.VisualBasic.IsNothing(objHeader) Then
+
                 Select Case header.dbch_devicetype
-                    Case DBT_DEVTYP.OEM : Me.HandleOEM(m)
+
+                    Case DBT_DEVTYP.OEM
+                        Me.HandleOEM(m)
+
                     Case DBT_DEVTYP.DEVNODE
-                    Case DBT_DEVTYP.VOLUME : Me.HandleVolume(m)
+                    Case DBT_DEVTYP.VOLUME
+                        Me.HandleVolume(m)
+
                     Case DBT_DEVTYP.PORT
                     Case DBT_DEVTYP.NET
                     Case DBT_DEVTYP.DEVICEINTERFACE
                     Case DBT_DEVTYP.HANDLE
+
                 End Select
+
             End If
+
         End Sub
 
         'Das Ereignis betrifft ein Volume
         Private Sub HandleVolume(ByRef m As Message)
+
             Dim volume As DEV_BROADCAST_VOLUME
             Dim objVolume As Object = m.GetLParam(volume.GetType)
+
             If Not Microsoft.VisualBasic.IsNothing(objVolume) Then
+
                 volume = DirectCast(objVolume, DEV_BROADCAST_VOLUME)
                 Dim di As New DriveInfo(Me.DriveFromMask(volume.dbcv_unitmask))
+
                 Select Case CInt(m.WParam)
-                    Case DBT_DEVICEARRIVAL : RaiseEvent DriveAdded(Me, di)
-                    Case DBT_DEVICEREMOVECOMPLETE : RaiseEvent DriveRemoved(Me, di)
+
+                    Case DBT_DEVICEARRIVAL
+                        RaiseEvent DriveAdded(Me, di)
+
+                    Case DBT_DEVICEREMOVECOMPLETE
+                        RaiseEvent DriveRemoved(Me, di)
+
                 End Select
+
             End If
+
         End Sub
 
         'OEM, und was genau?
         'Uns interesieren nur Volumes
         Private Sub HandleOEM(ByRef m As Message)
+
             Dim oem As DEV_BROADCAST_OEM
             Dim objOem As Object = m.GetLParam(oem.GetType)
+
             If Not Microsoft.VisualBasic.IsNothing(objOem) Then
+
                 oem = DirectCast(objOem, DEV_BROADCAST_OEM)
                 If oem.dbco_devicetype = DBT_DEVTYP.VOLUME Then Me.HandleVolume(m)
+
             End If
+
         End Sub
 
         'Liefert den Laufwerksbuchstaben zurück
         Private Function DriveFromMask(mask As Integer) As Char
+
             Dim result As Char = CChar(String.Empty)
+
             For b As Integer = 0 To 25
+
                 If (mask And CInt(2 ^ b)) <> 0 Then
+
                     result = Microsoft.VisualBasic.Chr(65 + b)
                     Exit For
+
                 End If
+
             Next b
+
             Return result
+
         End Function
 
         Public Sub New()
+
             Me.CreateHandle(New CreateParams) 'eigenes Handle erstellen
+
         End Sub
 
         Protected Overridable Sub Dispose(disposing As Boolean)
+
             If Not Me.disposedValue Then
+
                 If disposing Then
+
                     ' Verwalteten Zustand (verwaltete Objekte) bereinigen
                     Me.DestroyHandle() 'eigenes Handle zerstören
+
                 End If
+
                 ' Nicht verwaltete Ressourcen (nicht verwaltete Objekte) freigeben und Finalizer überschreiben
                 ' Große Felder auf NULL setzen
                 Me.disposedValue = True
+
             End If
+
         End Sub
 
         ' Finalizer nur überschreiben, wenn "Dispose(disposing As Boolean)"
         ' Code für die Freigabe nicht verwalteter Ressourcen enthält
         Protected Overrides Sub Finalize()
+
             ' Ändern Sie diesen Code nicht.
             ' Fügen Sie Bereinigungscode in der Methode "Dispose(disposing As Boolean)" ein.
             Me.Dispose(disposing:=False)
             MyBase.Finalize()
+
         End Sub
 
         Public Sub Dispose() Implements IDisposable.Dispose
+
             ' Ändern Sie diesen Code nicht.
             ' Fügen Sie Bereinigungscode in der Methode "Dispose(disposing As Boolean)" ein.
             Me.Dispose(disposing:=True)
             GC.SuppressFinalize(Me)
+
         End Sub
 
     End Class
