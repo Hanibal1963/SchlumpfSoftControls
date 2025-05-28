38a41e37fc3ead4630f63175d573bcf4bbf9fbb3
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-05-28 16:28:35 +0200

    Kommentare und Dokumentation in Helpers.vb verbessert
    Aktualisierung der Kommentare zur Verbesserung der Verständlichkeit der Funktionen.
    Die Erklärungen für die Initialisierung von Variablen, die Überprüfung von Frames pro Sekunde und den Zoomfaktor wurden präzisiert.
    Rückgabewerte und Parameter sind nun besser dokumentiert.
    Einige Codezeilen wurden umformuliert, um die Logik klarer darzustellen.
    Copyright-Jahre wurden aktualisiert.

--- AniGifControl/Helpers.vb
+++ AniGifControl/Helpers.vb
@@ -1,8 +1,13 @@
-﻿' ****************************************************************************************************************
+﻿' *************************************************************************************************
+' 
 ' Helpers.vb
-' © 2024 by Andreas Sauer
-' ****************************************************************************************************************
+' Copyright (c)2025 by Andreas Sauer 
 '
+' Kurzbeschreibung:
+' 
+' Dieses Modul enthält Hilfsfunktionen, die in der Ani Gif Control verwendet werden.
+'
+' *************************************************************************************************
 
 Imports System.Drawing
 
@@ -12,16 +17,16 @@ Imports System.Drawing
 Friend Module Helpers
 
     ''' <summary>
-    ''' Stellt die Variablen mit den Startbedingungen ein.
+    ''' Setzt die Standardwerte für die wichtigsten Variablen der Ani Gif Control.
     ''' </summary>
     Friend Sub InitializeValues()
 
-        Gif = My.Resources.Standard 'Standardanimation laden
-        Autoplay = False 'Standardeinstellung für AutoPlay
-        GifSizeMode = SizeMode.Normal 'Standarddarstellung für Grafik
-        CustomDisplaySpeed = False
-        FramesPerSecond = 10
-        ZoomFactor = 50  'Standardeinstellung für Zoomfaktor
+        Gif = My.Resources.Standard         ' Standard-GIF aus den Ressourcen laden
+        Autoplay = False                   ' Animation startet nicht automatisch
+        GifSizeMode = SizeMode.Normal      ' GIF wird in Originalgröße angezeigt
+        CustomDisplaySpeed = False         ' Keine benutzerdefinierte Geschwindigkeit
+        FramesPerSecond = 10               ' Standard: 10 Bilder pro Sekunde
+        ZoomFactor = 50                    ' Standard-Zoomfaktor: 50%
 
     End Sub
 
@@ -31,27 +36,48 @@ Friend Module Helpers
     ''' <param name="Frames"></param>
     Friend Function CheckFramesPerSecondValue(Frames As Decimal) As Decimal
 
+        ' Überprüft, ob der FPS-Wert im zulässigen Bereich liegt (1 bis 50)
         Select Case Frames
 
-            Case Is < 1 : Return 1
-            Case Is > 50 : Return 50
-            Case Else : Return Frames
+            Case Is < 1
+                ' Wenn der Wert kleiner als 1 ist, auf Mindestwert 1 setzen
+                Return 1
+            Case Is > 50
+                ' Wenn der Wert größer als 50 ist, auf Höchstwert 50 setzen
+                Return 50
+            Case Else
+                ' Ansonsten den übergebenen Wert zurückgeben
+                Return Frames
 
         End Select
 
     End Function
 
     ''' <summary>
-    ''' Püft den Zoomfaktor
+    ''' Prüft, ob der übergebene Zoomfaktor im gültigen Bereich (1-100) liegt.
+    ''' Werte kleiner als 1 werden auf 1 gesetzt, Werte größer als 100 auf 100.
     ''' </summary>
-    ''' <param name="ZoomFactor"></param>
+    ''' <param name="ZoomFactor">
+    ''' Der zu prüfende Zoomfaktor.
+    ''' </param>
+    ''' <returns>
+    ''' Den korrigierten Zoomfaktor im Bereich 1 bis 100.
+    ''' </returns>
     Friend Function CheckZoomFactorValue(ZoomFactor As Decimal) As Decimal
 
         Select Case ZoomFactor
 
-            Case Is < 0 : Return 1
-            Case Is > 100 : Return 100
-            Case Else : Return ZoomFactor
+            Case Is < 0
+                ' Wenn der Zoomfaktor kleiner als 0 ist, auf Mindestwert 1 setzen
+                Return 1
+
+            Case Is > 100
+                ' Wenn der Zoomfaktor größer als 100 ist, auf Höchstwert 100 setzen
+                Return 100
+
+            Case Else
+                ' Ansonsten den übergebenen Wert zurückgeben
+                Return ZoomFactor
 
         End Select
 
@@ -77,42 +103,43 @@ Friend Module Helpers
         Select Case Mode
 
             Case SizeMode.Normal
-                'Zeichenflächengröße an Grafik anpassen
+                ' Bild wird in Originalgröße angezeigt (keine Skalierung)
                 Return New Size(Gif.Size.Width, Gif.Size.Height)
 
             Case SizeMode.CenterImage
-                'Zeichenflächengröße an Grafik anpassen
+                ' Bild wird ebenfalls in Originalgröße angezeigt (zentriert, aber Größe bleibt gleich)
                 Return New Size(Gif.Size.Width, Gif.Size.Height)
 
             Case SizeMode.Zoom
-                'Zeichenflächengröße laut Zoomwert anpassen
+                ' Bild wird proportional zum Zoomfaktor skaliert
                 If Gif.Size.Width < Gif.Size.Height Then
-                    'Anpassung wenn Bild höher als breit
+                    ' Bild ist höher als breit:
+                    ' Höhe des Controls als Basis, Breite proportional berechnen und mit Zoom multiplizieren
                     Return New Size(
                         CInt(Control.Height / CDec(Gif.Size.Height / Gif.Size.Width) * Zoom),
                         CInt(Control.Height * Zoom))
-
                 Else
-                    'Anpassung wenn Bild breiter als hoch
+                    ' Bild ist breiter als hoch:
+                    ' Breite des Controls als Basis, Höhe proportional berechnen und mit Zoom multiplizieren
                     Return New Size(
                         CInt(Control.Width * Zoom),
                         CInt(Control.Width * CDec(Gif.Size.Height / Gif.Size.Width) * Zoom))
-
                 End If
 
             Case SizeMode.Fill
-                'Zeichenflächengröße auf 100% des Controls anpassen
+                ' Bild wird so skaliert, dass es das Control vollständig ausfüllt (Seitenverhältnis bleibt erhalten)
                 If Gif.Size.Width < Gif.Size.Height Then
-                    'Anpassung wenn Bild höher als breit
+                    ' Bild ist höher als breit:
+                    ' Höhe des Controls als Basis, Breite proportional berechnen
                     Return New Size(
                         CInt(Control.Height / CDec(Gif.Size.Height / Gif.Size.Width)),
                         Control.Height)
                 Else
-                    'Anpassung wenn Bild breiter als hoch
+                    ' Bild ist breiter als hoch:
+                    ' Breite des Controls als Basis, Höhe proportional berechnen
                     Return New Size(
                         Control.Width,
                         CInt(Control.Width * CDec(Gif.Size.Height / Gif.Size.Width)))
-
                 End If
 
         End Select
@@ -136,25 +163,30 @@ Friend Module Helpers
     ''' </param>
     Friend Function GetRectStartPoint(Mode As SizeMode, Control As AniGif, Gif As Bitmap, RectStartSize As Size) As Point
 
+        ' Bestimmt den Startpunkt (linke obere Ecke) für das Zeichnen des Bildes
         Select Case Mode
 
             Case SizeMode.Normal
-                'Startpunkt ist links oben
+                ' Bild wird in Originalgröße oben links gezeichnet
                 Return New Point(0, 0)
 
             Case SizeMode.CenterImage
-                'Startpunkt von links ist die Hälfte der Differenz aus Controlbreite und Bildbreite
-                'Startpunkt von oben ist die Hälfte der Differenz aus Controlhöhe und Bildhöhe
+                ' Bild wird in Originalgröße zentriert gezeichnet
+                ' X-Position: Hälfte der Differenz zwischen Control-Breite und Bild-Breite
+                ' Y-Position: Hälfte der Differenz zwischen Control-Höhe und Bild-Höhe
                 Return New Point(CInt((Control.Width - Gif.Size.Width) / 2),
                                  CInt((Control.Height - Gif.Size.Height) / 2))
 
             Case SizeMode.Zoom
-                '
+                ' Bild wird skaliert (gezoomt) und zentriert gezeichnet
+                ' X-Position: Hälfte der Differenz zwischen Control-Breite und skalierter Bild-Breite
+                ' Y-Position: Hälfte der Differenz zwischen Control-Höhe und skalierter Bild-Höhe
                 Return New Point(CInt((Control.Width - RectStartSize.Width) / 2),
                                  CInt((Control.Height - RectStartSize.Height) / 2))
 
             Case SizeMode.Fill
-                '
+                ' Bild wird so skaliert, dass es das Control ausfüllt und zentriert gezeichnet
+                ' X- und Y-Position wie bei Zoom
                 Return New Point(CInt((Control.Width - RectStartSize.Width) / 2),
                                  CInt((Control.Height - RectStartSize.Height) / 2))
 
