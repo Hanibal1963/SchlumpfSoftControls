fd283cc258538f784530ab8c2fd01b1d6dd3a5e2
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-05-28 16:27:20 +0200

    Variablen für GIF-Handling in VariableDefinitions.vb hinzugefügt
    In der Datei `VariableDefinitions.vb` wurden mehrere neue Variablen implementiert, um die Handhabung von GIF-Bildern im Ani Gif Control-Projekt zu verbessern. Die hinzugefügten Variablen umfassen `Gif`, `GifSizeMode`, `CustomDisplaySpeed`, `FramesPerSecond`, `Dimension`, `Frame`, `MaxFrame`, `Autoplay` und `ZoomFactor`. Diese Erweiterungen ermöglichen eine bessere Verwaltung und Anzeige von GIF-Animationen.

--- AniGifControl/VariableDefinitions.vb
+++ AniGifControl/VariableDefinitions.vb
@@ -1,8 +1,13 @@
-﻿' ****************************************************************************************************************
+﻿' *************************************************************************************************
+' 
 ' VariableDefinitions.vb
-' © 2024 by Andreas Sauer
-' ****************************************************************************************************************
+' Copyright (c) 2024-2025 by Andreas Sauer 
 '
+' Kurzbeschreibung:
+' 
+' Dieses Modul enthält die Definitionen der im Ani Gif Control-Projekt verwendeten Variablen.
+'
+' *************************************************************************************************
 
 Imports System.Drawing
 Imports System.Drawing.Imaging
@@ -12,14 +17,31 @@ Imports System.Drawing.Imaging
 ''' </summary>
 Friend Module VariableDefinitions
 
+    ' Das aktuell geladene GIF-Bild
     Friend Gif As Bitmap
+
+    ' Gibt an, wie das GIF im Steuerelement skaliert/angezeigt wird (z.B. gestreckt, zentriert)
     Friend GifSizeMode As SizeMode
+
+    ' Bestimmt, ob eine benutzerdefinierte Anzeigegeschwindigkeit verwendet wird
     Friend CustomDisplaySpeed As Boolean
+
+    ' Bildwiederholrate (Frames pro Sekunde) für die Animation
     Friend FramesPerSecond As Decimal
+
+    ' Die Dimension (z.B. Zeit) der animierten Frames im GIF
     Friend Dimension As FrameDimension
+
+    ' Der aktuelle Frame-Index, der angezeigt wird
     Friend Frame As Integer
+
+    ' Die maximale Anzahl der Frames im GIF
     Friend MaxFrame As Integer
+
+    ' Gibt an, ob die Animation automatisch abgespielt wird
     Friend Autoplay As Boolean
+
+    ' Zoomfaktor für die Anzeige des GIFs
     Friend ZoomFactor As Decimal
 
 End Module
